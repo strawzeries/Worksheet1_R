8a5f77ca644d8e10b04bb49b9057739517a88751
Barbie Joy Obas <barbiejoy.obas@students.isatu.edu.ph>
2023-09-20 03:55:59 +0000

    initial commit

---
+++ RWorksheet_Obas#1.R
@@ -0,0 +1,38 @@
+#Vector
+
+age <- c ( 34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 
+           22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 
+           42, 53, 41, 51, 35, 24, 33, 41)
+
+#1 data ponts
+  length(age)
+
+#2 reciprocal 
+  reciprocal <-1/age
+  reciprocal
+
+#3 Assign also new_age <- c(age, 0, age)
+  NewAge <- c (age, 0, age)
+  NewAge
+
+#4 Sort the values for age
+  SortValueAge <- sort(age)
+  SortValueAge
+  
+#5 minimum and maximum age
+  min_age <- min(age)
+  min_age
+  max_age <- max(age)
+  max_age
+  
+
+data <- c ( 2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+            2.5, 2.3, 2.4, 2.7 )
+#6 data points
+  length(data)
+  
+#7 New Vector for Data
+  DoubleData <- data * 2
+  DoubleData
+  
+  
