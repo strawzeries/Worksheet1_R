63f31f72b03ab39fa314238d551c2f287093acc9
Barbie Joy Obas <barbiejoy.obas@students.isatu.edu.ph>
2023-09-28 03:54:19 +0000

    initial commit

---
+++ RWorksheet_Obas#2.R
@@ -0,0 +1,44 @@
+# 1 operator
+#1.a
+operator <- seq(-5,5)
+operator
+# the output will be the numbers from -5 to 5
+
+
+#1.b
+x <- 1:7
+x
+# the value of will be the numbers from 1 to 7
+
+# 2
+seqq <- seq(1, 3, by=0.2)
+seqq
+#
+
+# 3
+age <- c ( 34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27,
+           22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 
+           43, 53, 41, 51, 35, 24, 33, 40, 18, 44, 38, 41, 48, 
+           27, 39, 19, 30, 61, 54, 58, 26, 18)
+# 3.a 
+Element3 <- age[3]
+Element3
+# 3.b
+Element2 <- age[2]
+Element4 <- age[4]
+
+Element2
+Element4
+
+# 4
+x <- c("first"=3, "second"=0, "third"=9)
+x
+
+x[c("first", "third")]
+
+# 5 
+x <- seq(-3,2)
+x
+
+  
+  
